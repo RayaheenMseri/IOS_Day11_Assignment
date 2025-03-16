43bb56e9c4c5bac4b4b2aaed3bfb9adcdd5692ce
RayaheenMseri <rayaheenmseri@gmail.com>
2025-03-16 11:34:40 +0300

    Initial Commit

---
+++ Day11_Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day11_Assignment
+//
+//  Created by Rayaheen Mseri on 16/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day11_Assignment/Day11_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day11_AssignmentApp.swift
+//  Day11_Assignment
+//
+//  Created by Rayaheen Mseri on 16/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Day11_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
